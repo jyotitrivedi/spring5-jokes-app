e6583a0853e1c2a3cbd25ce428e03f96e19a17c3
jyotitrivedi <jyotitrivedi99@gmail.com>
2020-05-07 17:31:35 +0530

    Completed App

---
+++ src/main/java/guru/springframework/jokes/controllers/JokeController.java
@@ -0,0 +1,24 @@
+package guru.springframework.jokes.controllers;
+
+import guru.springframework.jokes.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+    private final JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+
+        model.addAttribute("joke",jokeService.getQuotes());
+        return "chucknorries";
+    }
+}
